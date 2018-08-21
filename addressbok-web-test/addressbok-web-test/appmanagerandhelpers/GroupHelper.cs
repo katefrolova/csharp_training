aeb74155ce33c7a6899d8b168f87d569a413e1f1
E.Frolova <katr1na95@mail.ru>
2018-08-21 10:46:51 +0300

    Лекция 4.1. Коллекции (списки)

--- addressbok-web-test/addressbok-web-test/appmanagerandhelpers/GroupHelper.cs
+++ addressbok-web-test/addressbok-web-test/appmanagerandhelpers/GroupHelper.cs
@@ -28,6 +28,19 @@ namespace WebAddressbookTests
             return this;
         }
 
+        public List<GroupData> GetGroupList()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            manager.Navigator.GoToGroupsPage();
+            ICollection<IWebElement> elements = driver.FindElements(By.CssSelector("span.group"));
+            foreach (IWebElement element in elements)
+            {
+                //GroupData group = new GroupData(element.Text);
+                groups.Add(new GroupData(element.Text));
+            }
+            return groups;
+        }
+
         public GroupHelper Modify(int v, GroupData newData)
         {
             manager.Navigator.GoToGroupsPage();
