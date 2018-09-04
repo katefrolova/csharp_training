d68e266206af1e1327fae3e5054547bb640751c3
E.Frolova <katr1na95@mail.ru>
2018-09-04 09:26:33 +0300

    Лекция 6.4. Чтение и запись файлов в формате Excel

--- addressbok-web-test/addressbook-test-data-generators/Program.cs
+++ addressbok-web-test/addressbook-test-data-generators/Program.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Xml;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace addressbook_test_data_generators
 {
@@ -16,8 +17,8 @@ namespace addressbook_test_data_generators
         static void Main(string[] args)
         {
             int count = Convert.ToInt32(args[0]);
-            StreamWriter writer = new StreamWriter("args[1]");
-            string format = args[3];
+            string filename = args[1];
+            string format = args[2];
 
             List<GroupData> groups = new List<GroupData>();
             for (int i = 0; i < count; i++)
@@ -33,25 +34,59 @@ namespace addressbook_test_data_generators
                 //TestBase.GenerateRandomString(10)
                 //    ));
             }
-            if (format == "csv")
+            if(format == "excel")
             {
-                writeGroupsToCsvFile(groups, writer);
+                writeGroupsToExcelFile(groups, filename);
             }
-            else if (format == "xml")
-            {
-                writeGroupsToXmlFile(groups, writer);
-            }
-            else if (format == "json")
-            {
-                writeGroupsToJsonFile(groups, writer);
-            }
-
             else
             {
-                System.Console.Out.Write("Unrecognized format" + format);
+                StreamWriter writer = new StreamWriter("args[1]");
+                if (format == "csv")
+                {
+                    writeGroupsToCsvFile(groups, writer);
+                }
+                else if (format == "xml")
+                {
+                    writeGroupsToXmlFile(groups, writer);
+                }
+                else if (format == "json")
+                {
+                    writeGroupsToJsonFile(groups, writer);
+                }
+
+                else
+                {
+                    System.Console.Out.Write("Unrecognized format" + format);
+                }
+                writer.Close();
             }
-            writer.Close();
         }
+
+       static void writeGroupsToExcelFile(List<GroupData> groups, string filename)
+        {
+            Excel.Application app = new Excel.Application();
+            app.Visible = true;
+            app.Workbooks.Add();
+            Excel.Workbook wb = app.Workbooks.Add();
+            Excel.Worksheet sheet = wb.ActiveSheet;
+
+            int row = 1;
+            foreach (GroupData group in groups)
+            {
+                sheet.Cells[row, 1] = group.Name;
+                sheet.Cells[row, 2] = group.Header;
+                sheet.Cells[row, 3] = group.Footer;
+                row++;
+            }
+            string fullPath = Path.Combine(Directory.GetCurrentDirectory(), filename);
+            File.Delete(fullPath);
+            wb.SaveAs(fullPath);
+            wb.Close();
+            app.Visible = false;
+
+            app.Quit();
+        }
+
         static void writeGroupsToCsvFile(List<GroupData> groups, StreamWriter writer)
         {
             foreach(GroupData group in groups)
@@ -70,6 +105,8 @@ namespace addressbook_test_data_generators
         {
             writer.Write(JsonConvert.SerializeObject(groups,Newtonsoft.Json.Formatting.Indented));
         }
+
+
 
     }
 }
